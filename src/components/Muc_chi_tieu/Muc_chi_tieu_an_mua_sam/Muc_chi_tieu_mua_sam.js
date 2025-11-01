2ae69e3f9c5e09cb8ec7c8d9f6156767d014e5b8
nguyenhung252006 <nguyenviethungk56@gmail.com>
2025-11-01 15:28:50 +0700

    Update UI Muc chi tieu

--- src/components/Muc_chi_tieu/Muc_chi_tieu_an_mua_sam/Muc_chi_tieu_mua_sam.js
+++ src/components/Muc_chi_tieu/Muc_chi_tieu_an_mua_sam/Muc_chi_tieu_mua_sam.js
@@ -5,16 +5,23 @@ import style from './Muc_chi_tieu_mua_sam.module.scss'
 import ContentChiTieu from "../../../cong_cu/Text_chi_tieu/Text_chi_tieu";
 
 const cx = classNames.bind(style)
-function Muc_chi_tieu_mua_sam({children}) {
-    return ( 
+function Muc_chi_tieu_mua_sam({ children }) {
+    var list = Array.from({ length: 100 }, (_, i) => `Thông báo số dư #${i + 1}: Số dư hiện tại là ${1000 + i * 500} VNĐ`);
+    var item = list.map(item => {
+        return (
+            <p>{item}</p>
+        )
+    })
+    return (
         <div className={cx('wrapper')}>
             {<ContentChiTieu
                 tenMuc={'Mua Sắm'}
                 daSuDung={'10000'}
                 PhanTramDaSuDung={'10'}
+                daThem={item}
             />}
         </div>
-     );
+    );
 }
 
 export default Muc_chi_tieu_mua_sam;
--- src/cong_cu/Text_chi_tieu/Text_chi_tieu.js
+++ src/cong_cu/Text_chi_tieu/Text_chi_tieu.js
@@ -5,32 +5,71 @@ import style from './Text_chi_tieu.module.scss'
 import Card from "../Card/Card";
 
 const cx = classNames.bind(style)
-function Text_chi_tieu({ tenMuc, daSuDung, PhanTramDaSuDung }) {
+function Text_chi_tieu({ tenMuc, daSuDung, PhanTramDaSuDung, daThem, ghiChu }) {
     return (
         <>
-            <Card className={'wrapper-content'}>
-                <div className={cx('wrapper')}>
-                    <h1 className={cx('name-page')}>{tenMuc}</h1>
-                    <div className={cx('content')}>
-                        <p>Đã sử dụng: <span className={cx('use')}>{daSuDung}</span></p>
-                        <p>Số phần trăm đã sử dụng so với định mức (%): <span className={cx('phan-tram')}>{PhanTramDaSuDung}%</span></p>
-                    </div>
-                </div>
-            </Card>
-            <Card className={'wrapper-content'}>
-                <div className={'wrapper-content'}>
-                    <h1 className={cx('input-page')}>Thêm khoản chi</h1>
-                    <div className={cx('content')}>
-                        <div className={cx('input-submit')}>
-                            <div>
-                                <label htmlFor="so-tien"> nhập số tiền: </label>
-                                <input id="so-tien"></input>
-                                <button className={cx('btn-submit')}>Xác Nhận</button>
+            <div className={cx('wrapper-page')}>
+                <div>
+                    <Card className={'wrapper-content'}>
+                        <div className={cx('wrapper')}>
+                            <h1 className={cx('name-page')}>{tenMuc}</h1>
+                            <div className={cx('content')}>
+                                <p>Đã sử dụng: <span className={cx('use')}>{daSuDung}</span></p>
+                                <p>Số phần trăm đã sử dụng so với định mức (%): <span className={cx('phan-tram')}>{PhanTramDaSuDung}%</span></p>
                             </div>
                         </div>
-                    </div>
+                    </Card>
+                    <Card className={'wrapper-content'}>
+                        <div className={'wrapper-content'}>
+                            <h1 className={cx('input-page')}>Thêm khoản chi</h1>
+                            <div className={cx('content')}>
+                                <div className={cx('input-submit')}>
+                                    <div>
+                                        <label htmlFor="so-tien"> nhập số tiền: </label>
+                                        <input id="so-tien"></input>
+                                        <label htmlFor="ghi-chu"> Ghi chú ( nếu có ): </label>
+                                        <input id="ghi-chu"></input>
+                                        <button className={cx('btn-submit')}>Xác Nhận</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </Card>
                 </div>
-            </Card>
+                <Card className={'wrapper-content'}>
+                    <div className={cx('wrapper-history')}>
+                        <h1 style={{
+                            'margin': '12px 12px',
+                            'fontWeight': '600',
+                        }}>Lịch sử đã thêm</h1>
+                        <div style={{
+                            margin: '12px 12px',
+                            fontWeight: 600,
+                            width: '100%',
+                            display: 'flex',
+                            alignItems: 'center',
+                            gap: '6px',
+                        }}>
+                            <span style={{
+                                minWidth: '400px',
+                                color: 'green',
+                                minHeight: '100px',
+                                maxHeight: '400px',
+                                overflowY: 'auto',
+                                borderTop: '1px solid wheat',
+                                borderBottom: '1px solid wheat',
+                                borderLeft: '1px solid wheat',
+                                borderRight: '1px solid wheat',
+                                borderRadius: '10px',
+                                padding :'12px 12px',
+                                boxShadow: '0 0 12px rgb(253, 247, 247)',
+                            }}>{daThem}</span>
+                            {ghiChu && <span>Ghi chú: {ghiChu}</span>}
+                        </div>
+
+                    </div>
+                </Card >
+            </div >
         </>
     );
 }
